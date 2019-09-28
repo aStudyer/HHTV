b2d9e36464cc0acef2d2a7c46cadc737773634ef
aStudyer <astudyer@163.com>
2019-09-29 06:46:15 +0800

    添加全屏pop手势

--- HHTV/Classes/Main/Controller/NavigationController.swift
+++ HHTV/Classes/Main/Controller/NavigationController.swift
@@ -13,6 +13,7 @@ class NavigationController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addFullScreenPop()
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle{
@@ -22,5 +23,21 @@ class NavigationController: UINavigationController {
     override func pushViewController(_ viewController: UIViewController, animated: Bool) {
         viewController.hidesBottomBarWhenPushed = true
         super.pushViewController(viewController, animated: animated)
+    }
+}
+
+// MARK: - 私有方法
+extension NavigationController {
+    /// 添加全屏pop手势
+    private func addFullScreenPop() {
+        guard let targets = interactivePopGestureRecognizer?.value(forKey: "_targets") as? [NSObject] else {
+            return
+        }
+        let targetObjc = targets.first
+        let target = targetObjc?.value(forKey: "target")
+        let action = Selector(("handleNavigationTransition:"))
+        
+        let panGes = UIPanGestureRecognizer(target: target, action: action)
+        view.addGestureRecognizer(panGes)
     }
 }
